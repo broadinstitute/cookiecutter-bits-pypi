c631867f21979aea7548b60df0c42bae19731729
coreone <coreone@users.noreply.github.com>
2019-09-04 12:20:39 -0400

    Add the ability to have a BITS branded PyPi module (#3)
    * Add bits-specific pieces
    * Update the README as needed

---
+++ {{cookiecutter.project_repo}}/bits/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Initialize the bits/{{ cookiecutter.project_slug }} module."""
+
+# This Example class can be deleted.  It is just used to prove this module is setup correctly.
+class Example(object):
+    """Example class."""
+
+    def __init__(self):
+        """Initialize an Example class instance."""
+        print('Hello world!')
