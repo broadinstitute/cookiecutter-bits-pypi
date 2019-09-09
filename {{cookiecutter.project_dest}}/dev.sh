ab6431726c3dc33969fba480bf9edfa10d4a072d
coreone <coreone@users.noreply.github.com>
2019-09-09 16:54:56 -0400

    Scaraplate integration (#4)
    * Add scaraplate.yaml to enable scaraplate rollup updates
    * Restructure for scaraplate
    * Add .scaraplate.conf replay config builder template
    * Add VSCode settings file
    * Add .dockerignore

--- {{cookiecutter.project_dest}}/dev.sh
+++ {{cookiecutter.project_dest}}/dev.sh
@@ -18,7 +18,7 @@ fi
 pushd "$SCRIPT_DIR" >/dev/null || exit 1
 
 if ! $SUDO docker image ls | awk '{print $1":"$2}' | grep -q "^$DOCKER_IMAGE"; then
-    $SUDO docker build -t "$DOCKER_IMAGE" .
+    $SUDO docker build --pull -t "$DOCKER_IMAGE" .
 fi
 
 $SUDO docker run $TTY --rm -v "$SCRIPT_DIR":/usr/src "$DOCKER_IMAGE" "$@"
