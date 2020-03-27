2f2e66df880405b6260c90ca5302eb3295950933
Andrew Teixeira <teixeira@broadinstitute.org>
2020-03-27 12:48:12 -0400

    Improve dev.sh script

--- {{cookiecutter.project_dest}}/dev.sh
+++ {{cookiecutter.project_dest}}/dev.sh
@@ -5,6 +5,19 @@ SUDO=
 
 SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+build_image() {
+    echo "Building image $DOCKER_IMAGE"
+    pushd "$SCRIPT_DIR" >/dev/null || exit 1
+
+    $SUDO docker build --pull -t "$DOCKER_IMAGE" .
+    if [ $? -ne 0 ]; then
+        echo 'Build failed.  Exiting!'
+        exit 2
+    fi
+
+    popd >/dev/null || exit 1
+}
+
 if [ "$TERM" != 'dumb' ] ; then
     TTY='-it'
 fi
@@ -15,12 +28,26 @@ if [ "$( uname -s )" != 'Darwin' ]; then
     fi
 fi
 
-pushd "$SCRIPT_DIR" >/dev/null || exit 1
+REBUILD=
+while getopts "r" OPTION; do
+    case $OPTION in
+        r)
+            REBUILD='yes'
+            shift
+            ;;
+        \?)
+            echo 'Invalid argument.'
+            exit 1
+            ;;
+    esac
+done
 
-if ! $SUDO docker image ls | awk '{print $1":"$2}' | grep -q "^$DOCKER_IMAGE"; then
-    $SUDO docker build --pull -t "$DOCKER_IMAGE" .
+if ! $SUDO docker image ls | awk '{print $1":"$2}' | grep -q "$DOCKER_IMAGE"; then
+    build_image
+fi
+
+if [ -n "$REBUILD" ]; then
+    build_image
 fi
 
 $SUDO docker run $TTY --rm -v "$SCRIPT_DIR":/usr/src "$DOCKER_IMAGE" "$@"
-
-popd >/dev/null || exit 1
