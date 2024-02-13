473bebd08e49fbc3f1a525f9653ab005f3798695
coreone <coreone@users.noreply.github.com>
2024-02-13 14:37:00 -0500

    feat: template updates (#32)
    chore: update pre-commit versions
    fix: remove old .pylintrc file
    chore: update README.md
    chore: update version and file paths for shared workflows
    feat: add updated dev.sh script to use podman if docker doesn't exist, as well as autobuilds
    fix: add --no-root option to `poetry install` in Dockerfile

--- {{cookiecutter.project_dest}}/dev.sh
+++ {{cookiecutter.project_dest}}/dev.sh
@@ -1,55 +1,88 @@
 #!/bin/bash
 
-DOCKER='docker' # Change to podman if you want to use podman
 DOCKER_IMAGE='{{ cookiecutter.project_slug }}:dev'
+LABEL_PREFIX='org.broadinstitute.{{ cookiecutter.project_slug }}'
+{% if cookiecutter.use_pipenv == 'y' %}TRACK_FILES=( Dockerfile Pipfile )
+{% endif -%}
+{% if cookiecutter.use_poetry == 'y' %}TRACK_FILES=( Dockerfile poetry.lock pyproject.toml )
+{% endif -%}
 SUDO=
 
 SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-build_image() {
-    echo "Building image $DOCKER_IMAGE"
-    pushd "$SCRIPT_DIR" >/dev/null || exit 1
+if which sha256sum >/dev/null; then
+    CHKSUM='sha256sum'
+elif which shasum >/dev/null; then
+    CHKSUM='shasum -a 256'
+elif which md5sum >/dev/null; then
+    CHKSUM='md5sum'
+else
+    echo 'Could not find a checksumming program. Exiting!'
+    exit 1
+fi
 
-    if ! $SUDO docker build --pull -t "$DOCKER_IMAGE" .; then
-        echo 'Build failed.  Exiting!'
+if which docker >/dev/null; then
+    DOCKER='docker'
+elif which podman >/dev/null; then
+    DOCKER='podman'
+else
+    echo 'Container environment cannot be found. Exiting!'
+    exit 2
+fi
+
+function build() {
+    if [ -z "$1" ]; then
+        echo 'Image name not provided to build. Exiting!'
+        exit 1
+    fi
+    DOCKER_IMAGE=$1
+
+    if [[ $(git diff --stat) != '' ]]; then
+        echo 'Branch is dirty.  The build can only happen on an unmodified branch.'
         exit 2
     fi
 
-    popd >/dev/null || exit 1
+    LABELS=
+    for tfile in "${TRACK_FILES[@]}"; do
+        label="$( $CHKSUM "$tfile" | awk -v PREFIX="$LABEL_PREFIX" '{print PREFIX"."$2"="$1}' )"
+        LABELS+="--label ${label} "
+    done
+
+    $SUDO $DOCKER build --pull -t "$DOCKER_IMAGE" $LABELS .
 }
+
 
 if [ "$TERM" != 'dumb' ] ; then
     TTY='-it'
 fi
 
 if [ "$( uname -s )" != 'Darwin' ]; then
-    if [ ${DOCKER} = 'docker' ]; then
-        if [ ! -w "$DOCKER_SOCKET" ]; then
-            SUDO='sudo'
-        fi
+    if [ ! -w "$DOCKER_SOCKET" ]; then
+        SUDO='sudo'
     fi
 fi
 
-REBUILD=
-while getopts "r" OPTION; do
-    case $OPTION in
-        r)
-            REBUILD='yes'
-            shift
-            ;;
-        \?)
-            echo 'Invalid argument.'
-            exit 1
-            ;;
-    esac
+pushd "$SCRIPT_DIR" >/dev/null || exit 1
+IMG_TEST="$( $SUDO $DOCKER image ls | awk '{print $1":"$2}' | grep "^${DOCKER_IMAGE}" )"
+if [ -z "$IMG_TEST" ]; then
+    build "$DOCKER_IMAGE"
+fi
+
+rebuild='0'
+for tfile in "${TRACK_FILES[@]}"; do
+    current="$( $CHKSUM "$tfile" | cut -d' ' -f1 )"
+    stored="$( $DOCKER image inspect --format="{% raw %}{{index .Config.Labels \"${LABEL_PREFIX}.${tfile}\"}}{% endraw %}" "$DOCKER_IMAGE" )"
+
+    if [ "$current" != "$stored" ]; then
+        echo "$tfile changed.  Rebuilding."
+        rebuild='1'
+    fi
 done
 
-if [ -n "$REBUILD" ]; then
-    build_image
-else
-    if ! $SUDO $DOCKER image ls | awk '{print $1":"$2}' | grep -q "^$DOCKER_IMAGE"; then
-        build_image
-    fi
+if [ "$rebuild" == "1" ]; then
+    build "$DOCKER_IMAGE"
 fi
 
-$SUDO $DOCKER run $TTY --rm -v "$SCRIPT_DIR":/usr/src "$DOCKER_IMAGE" "$@"
+$SUDO $DOCKER run $TTY --rm -v "$SCRIPT_DIR":/working "$DOCKER_IMAGE" "$@"
+
+popd >/dev/null || exit 1
